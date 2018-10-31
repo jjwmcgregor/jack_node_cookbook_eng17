99f72b48d43a43cc199425fb1a5791dc7eb9df53
Jack <jmcgregor@spartaglobal.com>
2018-10-31 11:58:28 +0000

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+# the attributes we can update our node install to the latest version by overriding the following attributes
+default['nodejs']['repo']         = 'https://deb.nodesource.com/node_8.x'
+default['nodejs']['version'] = '8.11.2'
+default['nginx']['proxy_port'] = 3000
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -7,12 +7,36 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
     it { should exist }
   end
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+describe service "nginx" do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+# Our testing is pretty basic at the moment. We know that Nginx is listening on port 80 but that doesn't fully describe what it's supposed to do. If our configuration is correct it should really forward us to port 3000. We saw previously that if there's nothing running on port 3000 we should get a 502 Bad Gateway status error returned. We can test for that
+
+describe http('http://localhost', enable_remote_worker: true) do
+  its('status') { should cmp 502 }
+end
+
+describe package ('nodejs') do
+  it { should be_installed }
+  its('version') { should cmp > '8.11.2*' }
+end
+
+describe npm ("pm2") do
+  it { should be_installed }
 end
